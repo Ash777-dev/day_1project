af2ab349da016c4c91c48ff048c50ed2948c5b13
Ash777-dev <dummy@gmail.com>
2026-01-30 13:12:32 +0000

    solve initial commit

---
+++ server/src/main/java/com/edutech/progressive/dao/StudentDAOImpl.java
@@ -0,0 +1,25 @@
+package com.edutech.progressive.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.edutech.progressive.entity.Student;
+
+public class StudentDAOImpl {
+    public int addStudent(Student student){
+        return -1;
+    }
+    public Student getStudentId(int studentId){
+        return null;
+    }
+    public void updateStudent(Student student){
+
+    }
+    public void deleteStudent(int studentId){
+
+    }
+    public List<Student> getAllStudents(){
+        return new ArrayList<>();
+    }
+
+}
---
+++ server/src/main/java/com/edutech/progressive/entity/Course.java
@@ -0,0 +1,53 @@
+package com.edutech.progressive.entity;
+
+public class Course {
+    private int courseId;
+    private String courseName;
+    private String description;
+    private int teacherId;
+
+    public Course(){
+
+    }
+
+    public Course(int courseId, String courseName, String description, int teacherId) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.description = description;
+        this.teacherId = teacherId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+    
+
+}
